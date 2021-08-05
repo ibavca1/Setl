317479a6380bb69cbbaedc59c8a060376c04b09e
Anton <ibavca@gmail.com>
2021-08-05 16:21:43 +0500

    1

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,8 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Setl.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Setl
 {
