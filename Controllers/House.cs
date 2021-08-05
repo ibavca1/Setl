2aeb0776fe5fe9875b8640f66243673f0ab71a55
Anton <ibavca@gmail.com>
2021-08-05 09:32:02 +0500

    Добавьте файлы проекта.

---
+++ Controllers/House.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Setl.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class House : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Get(string token)
+        {
+            var house = new List<string>();
+            return Ok(house);
+        }
+    }
+}
